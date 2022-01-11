80018e17fbed16a40f79aaae58fa439b9872e16f
hirossan4049 <haruto405329@gmail.com>
2022-01-11 21:50:58 +0900

    [update](): Homeのデザインよくした

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,6 +12,7 @@ import { fetchLatestTracklists } from "../../actions/api";
 export const Home = () => {
 
     const [tracklists, setTracklists] = useState<[TrackListsType]>()
+    const [isLoading, setIsLoading] = useState(true)
 
     document.title = "śvara tracklists"
 
@@ -20,40 +21,48 @@ export const Home = () => {
 
         axios.get<[TrackListsType]>("https://s-vara.herokuapp.com/latest-tracklists")
         .then(res => {
-            console.log("res.data", res.data)
-          setTracklists(res.data)
-          tracklists?.map((value, index) => {
-            console.log(value.trackName, index)
-          })
+            setTracklists(res.data)
+            setIsLoading(false)
         })
       }, [])
 
 
     return (
         <>
-            <Box bgGradient='linear(to-l, red.400, pink.400)' h="900" p={{md: 32, base: 4}} verticalAlign={"center"} transform={"skewY(-3deg)"} transformOrigin={"top left"} >
+            <Box bgGradient='linear(to-l, red.400, pink.400)' h={{md: "900", base: "300"}} p={{md: 32, base: 4}} verticalAlign={"center"} transform={"skewY(-3deg)"} transformOrigin={"top left"} >
                 <Box transform={"skewY(3deg)"}>
-                    <Image src={"svara-logo.svg"} alt="svara logo" w={32} />
+                    <Image src={"svara-logo.svg"} alt="svara logo" w={{md: 32, base: 24}} />
 
             <Center>
-            <Image mt={32} src={"svara-logo.svg"} alt="svara logo" h={20} />
+            <Image mt={{md: 32, base: 0}} src={"svara-logo.svg"} alt="svara logo" h={{md: 20, base: 0}} />
             </Center>
-            <Text mt={10} fontSize={"2xl"} color={"gray.100"} fontWeight={"bold"} textAlign={"center"} >ಸ್ವರ śvaraはYoutubeに上がっている音楽ライブのトラックリストをより見やすくしたWebアプリです</Text>
+            <Text mt={10} fontSize={{md: "2xl", base: "xl"}} color={"gray.100"} fontWeight={"bold"} textAlign={"center"} >ಸ್ವರ śvaraはYoutubeに上がっている音楽ライブのトラックリストをより見やすくしたWebアプリです</Text>
             <Center>
-                <InputGroup mt={28} w="500px" >
-                    <Input bg={"white"} rounded={"full"} h={16} border={"none"} boxShadow={"red"} fontWeight={"bold"} shadow={"xl"} fontSize={"24"} placeholder='Youtube動画URLまたはIDを入力' />
-                    <InputRightElement h={16} children={<IconButton aria-label="Search" icon={<Icon as={IoArrowForward} w={8} h={8} />} bgColor={"white"} color="red.400" mr={4} rounded={"full"} onClick={() => {console.log("clicked")}} />} />
+                <InputGroup mt={{md: 28, base: 6}} w="500px" >
+                    <Input bg={"white"} rounded={"full"} h={{md: 16, base: 12}} border={"none"} boxShadow={"red"} fontWeight={"bold"} shadow={"xl"} fontSize={{md: "24", base: "18"}} placeholder='Youtube動画URLまたはIDを入力' />
+                    <InputRightElement h={{md: 16, base: 12}} children={<IconButton aria-label="Search" icon={<Icon as={IoArrowForward} w={{md: 8, base: 6}} h={{md: 8, base: 6}} />} bgColor={"white"} color="red.400" mr={4} rounded={"full"} onClick={() => {console.log("clicked")}} />} />
                 </InputGroup>
             </Center>
             </Box>
             </Box>
 
             <Center>
-                <Box p={{base: 4, md: 32}} w={{base: "none",md: "6xl"}}>
-                    <Text fontSize={"2xl"} fontWeight={"bold"} pl={6} >最近追加されたトラックリスト</Text>
-                    { tracklists?.map((value) => (
-                        <HomeTrackItem title={value.trackName} artist={"アーティスト: 不明"} href={"/tracklists/" + value.trackId} />
-                        ))}
+                <Box p={{base: 1, md: 32}} w={{base: "none",md: "6xl"}} mt={{base: 8, md: "0"}} width="100%">
+                    <Text fontSize={{base: "xl", md: "2xl"}} fontWeight={"bold"} pl={{base: 2, md: 6}} >最近追加されたトラックリスト</Text>
+                    { isLoading ?
+                        <Box>
+                        <HomeTrackItem isLoading={true} title={""} artist={""} href={""} />
+                        <HomeTrackItem isLoading={true} title={""} artist={""} href={""} />
+                        <HomeTrackItem isLoading={true} title={""} artist={""} href={""} />
+                        <HomeTrackItem isLoading={true} title={""} artist={""} href={""} />
+                        <HomeTrackItem isLoading={true} title={""} artist={""} href={""} />
+                        <HomeTrackItem isLoading={true} title={""} artist={""} href={""} />
+                        </Box>
+                        :
+                        tracklists?.map((value) => (
+                            <HomeTrackItem title={value.trackName} artist={"アーティスト: 不明"} href={"/tracklists/" + value.trackId} />
+                            ))
+                    }
                 </Box>
             </Center>
         </>
--- src/pages/home/components/HomeTrackItem.tsx
+++ src/pages/home/components/HomeTrackItem.tsx
@@ -2,18 +2,18 @@ import * as React from "react"
 import { Box, Text, LinkBox, LinkOverlay, Skeleton } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-export const HomeTrackItem = ({ title, artist, isLoading, href}: {title: string, artist: string, isLoading?: boolean, href: string}) => {
+export const HomeTrackItem = ({ title = "", artist = "", isLoading, href = ""}: {title: string, artist: string, isLoading?: boolean, href: string}) => {
     return (
-        <LinkBox as='article' rounded="xl" bg={ "white" } shadow="sm" p={4} m={4}>
+        <LinkBox as='article' rounded="xl" bg={ "white" } shadow="sm" p={{base: 2, md: 4}} m={{base: 1, md: 4}} mb={4} mt={4}>
             { isLoading ?
             <Box textAlign={"left"} >
-                <Skeleton mb={4} h={"1rem"} />
-                <Skeleton mb={4} h={"1rem"} w={20} />
+                <Skeleton mb={2} h={"1rem"} />
+                <Skeleton mb={2} h={{base: 4, md: 5}} w={20} />
             </Box> :
             <Link to={href} >
                 <Box textAlign={"left"}>
-                    <Text fontSize='xl' fontWeight="bold" color="gray.600"> {title} </Text>
-                    <Text fontSize='sm' fontWeight="bold" color="gray.500" h={5}> {artist} </Text>
+                    <Text fontSize={{base: "md", md: 'xl'}} fontWeight="bold" color="gray.600"> {title} </Text>
+                    <Text fontSize={{base: "12", md: 'sm'}} fontWeight="bold" color="gray.500" h={{base: 4, md: 5}}> {artist} </Text>
                 </Box>
             </Link>
             }
